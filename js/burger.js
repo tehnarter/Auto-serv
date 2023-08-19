83d222afd168efe643230c3f25dbed71ea725423
tehnar <tehnarter@gmail.com>
2023-08-19 12:30:19 +0300

    add-js

---
+++ js/burger.js
@@ -0,0 +1,6 @@
+document
+	.querySelector('.header__burger')
+	.addEventListener('click', function () {
+		this.classList.toggle('active')
+		document.querySelector('.header__nav').classList.toggle('open')
+	})
